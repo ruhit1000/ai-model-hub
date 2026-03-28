50cd282a87525476d183a5e8abad4db61aab7246
Ruhit Paul <ruhitpaul18@gmail.com>
2026-03-28 19:57:59 +0600

    Navbar, Banner, Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import NavBar from './components/NavBar/NavBar';
+import Banner from './components/Banner/Banner';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <NavBar />
+      <Banner />
+
+      <Footer />
+    </>
   );
 };
 
